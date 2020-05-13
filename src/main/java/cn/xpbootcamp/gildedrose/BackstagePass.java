7f0e4dee32b04f1b8554aadc26197768be8895dc
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-05-13 22:01:31 +0800

    Given 后台门票SellIn = 20, Quality = 5, When 过去了12天, 更新信息, Then SellIn = 8, Quality = 19

--- src/main/java/cn/xpbootcamp/gildedrose/BackstagePass.java
+++ src/main/java/cn/xpbootcamp/gildedrose/BackstagePass.java
@@ -1,13 +1,20 @@
 package cn.xpbootcamp.gildedrose;
 
-public class BackstagePass extends Good{
+public class BackstagePass extends Good {
     public BackstagePass(int sellIn, int quality) {
         super(sellIn, quality);
     }
 
     @Override
     void update(int numOfDay) {
+        int sellIn = super.getSellIn();
+        int quality = super.getQuality();
+        int resultOfSellIn = sellIn - numOfDay;
+        if (resultOfSellIn < 10) {
+            super.setQuality(quality + 10 + (10 - resultOfSellIn) * 2);
+        } else {
+            super.setQuality(super.getQuality() + numOfDay);
+        }
         super.setSellIn(super.getSellIn() - numOfDay);
-        super.setQuality(super.getQuality() + numOfDay);
     }
 }
--- src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
@@ -13,4 +13,12 @@ class BackstagePassTest {
         assertEquals(15, backstagePass.getSellIn());
         assertEquals(10, backstagePass.getQuality());
     }
+
+    @Test
+    void should_set_sell_in_8_and_quality_19_given_backstage_pass_sell_in_20_and_quality_5_when_pass_12_days() {
+        BackstagePass backstagePass = new BackstagePass(20, 5);
+        backstagePass.update(12);
+        assertEquals(8, backstagePass.getSellIn());
+        assertEquals(19, backstagePass.getQuality());
+    }
 }
