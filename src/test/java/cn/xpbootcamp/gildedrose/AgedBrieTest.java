d6116236dc75ebf7a77fe3ed76f36c97d3e4c9ed
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-05-17 18:56:35 +0800

    should_set_sell_in_nagetive_1_and_quality_51_given_general_good_sell_in_0_and_quality_50_when_pass_1_days

--- src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
@@ -23,10 +23,10 @@ class AgedBrieTest {
     }
 
     @Test
-    void should_set_sell_in_nagetive_11_and_quality_50_given_general_good_sell_in_20_and_quality_25_when_pass_31_days() {
-        AgedBrie agedBrie = new AgedBrie(20, 25);
-        agedBrie.update(31);
-        assertEquals(-11, agedBrie.getSellIn());
-        assertEquals(50, agedBrie.getQuality());
+    void should_set_sell_in_nagetive_1_and_quality_51_given_general_good_sell_in_0_and_quality_50_when_pass_1_days() {
+        AgedBrie agedBrie = new AgedBrie(0, 51);
+        agedBrie.update(1);
+        assertEquals(-1, agedBrie.getSellIn());
+        assertEquals(51, agedBrie.getQuality());
     }
 }
